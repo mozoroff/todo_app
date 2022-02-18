77bc9d6bc228410ee1b5a2dc081f44d12e86873d
mozoroff <mozoshima@gmail.com>
2022-02-18 11:43:41 +0900

    todoへ追加、変更、削除機能を追加

--- src/main/java/com/mozoroff/todo_app/controller/TodoController.java
+++ src/main/java/com/mozoroff/todo_app/controller/TodoController.java
@@ -2,13 +2,14 @@ package com.mozoroff.todo_app.controller;
 
 import java.util.List;
 
-import com.mozoroff.todo_app.domain.Todo;
-import com.mozoroff.todo_app.mapper.TodoMapper;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mozoroff.todo_app.domain.Todo;
+import com.mozoroff.todo_app.mapper.TodoMapper;
 
 @Controller
 public class TodoController {
@@ -16,11 +17,36 @@ public class TodoController {
     @Autowired
     TodoMapper todoMapper;
 
-    @RequestMapping(value = "/")
+    @RequestMapping("/")
     public String index(Model model) {
-        List<Todo> list = todoMapper.selectAll();
-        model.addAttribute("todos", list);
+//        List<Todo> list = todoMapper.selectAll();
+        List<Todo> incompleteList = todoMapper.selectIncomplete();
+        List<Todo> completeList = todoMapper.selectComplete();
+        
+        model.addAttribute("todos", incompleteList);
+        model.addAttribute("doneTodos", completeList);
+        
         return "index";
     }
+    
+    @PostMapping("/add")
+    public String add(Todo todo) {
+    	todoMapper.add(todo);
+    	System.out.println(todo);
+    	return "redirect:/";
+    }
+    
+    @PostMapping("/update")
+    public String update(Todo todo) {
+    	todoMapper.update(todo);
+    	return "redirect:/";
+    }
+    
+    @PostMapping("/delete")
+    public String delete() {
+    	todoMapper.delete();
+    	return "redirect:/";
+    }
+
 
 }
--- src/main/java/com/mozoroff/todo_app/mapper/TodoMapper.java
+++ src/main/java/com/mozoroff/todo_app/mapper/TodoMapper.java
@@ -1,11 +1,19 @@
 package com.mozoroff.todo_app.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 
 import com.mozoroff.todo_app.domain.Todo;
 
 @Mapper
 public interface TodoMapper {
     List<Todo> selectAll();
+    List<Todo> selectIncomplete();
+    List<Todo> selectComplete();
+    
+    void add(Todo todo);
+    void update(Todo todo);
+    void delete();
+    
 }
